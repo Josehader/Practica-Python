75ca3c22004bd08d8df9afc03fbf5b531067b91e
Josehader <jose.carpavire04@gmail.com>
2026-02-11 16:06:06 -0400

    Add comments and variable definitions to ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,24 @@
 #Práctica de Python#
+#url de la página oficial de python: https://www.python.org/#
+
+#Esto es un comentario#
+
+'''
+Esto es un comentario en una línea
+'''
+
+'''
+Esto es un 
+comentario 
+en varias 
+líneas
+'''
+
+#Variables#
+float # 1.5 ('Decimal')#
+int #1 ('Entero')#
+bool #True ('Decisión')#
+bool #False ('Decisión')#
+str #('Texto')#
+
+print("Hola mundo, Python")
