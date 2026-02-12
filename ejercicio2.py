8ea979191d181f94567d45113925132a4d893bfa
Josehader <jose.carpavire04@gmail.com>
2026-02-12 13:48:54 -0400

    Add ejercicio2.py with examples of operators and comparisons in Python

---
+++ ejercicio2.py
@@ -0,0 +1,25 @@
+# Tipos de Operadores en Python
+
+sum = 5 + 3 == 8
+difference = 5 - 3 == 2
+product = 5 * 3 == 15    
+quotient = 5 / 3 == 1.6666666666666667
+floor_division = 5 // 3 == 1
+modulus = 5 % 3 == 2
+exponentiation = 5 ** 3 == 125
+
+# Operadores de Comparación
+equal = 5 == 3 == False
+less_than = 5 < 3 == False
+greater_than = 5 > 3 == True
+less_equal = 5 <= 3 == False
+greater_equal = 5 >= 3 == True
+
+# Ejemplos de uso de operadores
+if 5 > 3:
+    print("5 es mayor que 3") # Esto se imprimirá porque la condición es verdadera
+
+# (Ejemplo mio para practicar)
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad") # Esto se imprimirá porque la condición es verdadera
